a800485bafb2981b6b4404ed52a1fc8cdd4b17e7
Mohamed ER-Radi <erradi1994mohamed@gmail.com>
2020-04-13 14:38:32 +0200

    Checkboxes - Spring MVC Form Tags

--- src/com/lov2code/springdemo/mvc/Student.java
+++ src/com/lov2code/springdemo/mvc/Student.java
@@ -16,6 +16,8 @@ public class Student {
     private String favoriteLanguage;
     private LinkedHashMap<String, String> favoriteLanguageOptions;
 
+    private String[] operatingSystems;
+
     public Student() {
         // populate country options: used ISO country code
         /*
@@ -78,5 +80,13 @@ public class Student {
 
     public LinkedHashMap<String, String> getFavoriteLanguageOptions() {
         return favoriteLanguageOptions;
+    }
+
+    public String[] getOperatingSystems() {
+        return operatingSystems;
+    }
+
+    public void setOperatingSystems(String[] operatingSystems) {
+        this.operatingSystems = operatingSystems;
     }
 }
